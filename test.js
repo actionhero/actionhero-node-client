1dc721eacc584e8b964791d3cb442761df6904d8
Evan Tahler <evantahler@gmail.com>
2013-01-12 18:01:06 -0800

    updates for new AH version

--- test.js
+++ test.js
@@ -8,24 +8,24 @@ var action_hero_client = require("./actionhero_client.js");
 var A = new action_hero_client;
 
 var connectionParams = {
-	host: "0.0.0.0",
-	port: "5678",
+  host: "0.0.0.0",
+  port: "5678",
   timeout: 1000,
 };
 
 // Create an actionHero server on testing ports
 var startServer = function(next){
-	var actionHeroPrototype = require("actionHero").actionHeroPrototype;
+  var actionHeroPrototype = require("actionHero").actionHeroPrototype;
   var actionHero = new actionHeroPrototype();
-	var params = {};
-	params.configChanges = {
-		general: {
-			workers: 1
-		},
-		log: {
-			logging: false,
-		},
-		httpServer: {
+  var params = {};
+  params.configChanges = {
+    general: {
+      workers: 1
+    },
+    log: {
+      logging: false,
+    },
+    httpServer: {
       enable: false,
     },
     tcpServer: {
@@ -33,31 +33,31 @@ var startServer = function(next){
       secure: false,
       port: 5678
     },
-		webSockets: {
-			enable: false
-		},
-		redis : {
-			enable: false,
-		}
-	}
-	actionHero.start(params, function(err, api){
-		console.log("Boot Sucessful!");
-		next(null, api);
-	});
+    webSockets: {
+      enable: false
+    },
+    redis : {
+      enable: false,
+    }
+  }
+  actionHero.start(params, function(err, api){
+    console.log("Boot Sucessful!");
+    next(null, api);
+  });
 }
 
 // test it! 
 suite.addBatch({
   "I should be able to start an actionHero Server": {
     topic: function(){
-    	var cb = this.callback
-    	startServer(function(err, server_api){
-    		api = server_api;
-    		cb(true, api)
-    	});
+      var cb = this.callback
+      startServer(function(err, server_api){
+        api = server_api;
+        cb(true, api)
+      });
     },
     'I got the API object' : function(res, api){ 
-    	assert.isObject(api); 
+      assert.isObject(api); 
     }
   }
 });
@@ -65,14 +65,14 @@ suite.addBatch({
 suite.addBatch({
   "The client should be able to connect": {
     topic: function(){
-    	var cb = this.callback
-    	A.on("connected", function(){
-    		cb(true, "connected")
-    	});
-    	A.connect(connectionParams);
+      var cb = this.callback
+      A.on("connected", function(){
+        cb(true, "connected")
+      });
+      A.connect(connectionParams);
     },
     'connected?' : function(res, msg){ 
-    	assert.strictEqual(msg, "connected"); 
+      assert.strictEqual(msg, "connected"); 
     }
   }
 });
@@ -87,7 +87,7 @@ suite.addBatch({
     },
     'details?' : function(res, details, delta){ 
       assert.strictEqual(details.status, "OK");
-      assert.isObject(details.details.public);
+      assert.isObject(details.details);
       assert.strictEqual(delta >= 0, true);
       assert.strictEqual(delta < 1000, true);
     }
@@ -97,12 +97,12 @@ suite.addBatch({
 suite.addBatch({
   "Server messages should be logged": {
     topic: function(){
-    	var cb = this.callback;
-    	cb(true, A.log);
+      var cb = this.callback;
+      cb(true, A.log);
     },
     'log?' : function(res, log){ 
-    	var str = log[0].data.welcome;
-    	assert.strictEqual(str, "Hello! Welcome to the actionHero api"); 
+      var str = log[0].data.welcome;
+      assert.strictEqual(str, "Hello! Welcome to the actionHero api"); 
     }
   }
 });
@@ -110,13 +110,13 @@ suite.addBatch({
 suite.addBatch({
   "I should be in a chat room": {
     topic: function(){
-    	var cb = this.callback;
-    	A.roomView(function(err, msg){
-    		cb(err, msg)
-    	})
+      var cb = this.callback;
+      A.roomView(function(err, msg){
+        cb(err, msg)
+      })
     },
     'roomStatus?' : function(res, msg){ 
-    	assert.strictEqual(msg.room, "defaultRoom"); 	
+      assert.strictEqual(msg.room, "defaultRoom");  
     }
   }
 });
@@ -124,15 +124,15 @@ suite.addBatch({
 suite.addBatch({
   "I can set and view params": {
     topic: function(){
-    	var cb = this.callback;
-    	A.paramAdd("key", "value", function(err, msg){
-    		A.paramsView(function(err, params){
-    			cb(err, params)
-    		});
-    	})
+      var cb = this.callback;
+      A.paramAdd("key", "value", function(err, msg){
+        A.paramsView(function(err, params){
+          cb(err, params)
+        });
+      })
     },
     'params?' : function(res, params){ 
-    	assert.strictEqual(params.params.key, "value");
+      assert.strictEqual(params.params.key, "value");
     }
   }
 });
@@ -140,15 +140,15 @@ suite.addBatch({
 suite.addBatch({
   "I can delete (and confirm gone) params": {
     topic: function(){
-    	var cb = this.callback;
-    	A.paramsDelete(function(err, msg){
-    		A.paramsView(function(err, params){
-    			cb(err, params)
-    		});
-    	})
+      var cb = this.callback;
+      A.paramsDelete(function(err, msg){
+        A.paramsView(function(err, params){
+          cb(err, params)
+        });
+      })
     },
     'no params?' : function(res, params){ 
-    	assert.strictEqual(params.params.key, undefined);
+      assert.strictEqual(params.params.key, undefined);
     }
   }
 });
@@ -156,15 +156,15 @@ suite.addBatch({
 suite.addBatch({
   "I can run an action (simple)": {
     topic: function(){
-    	var cb = this.callback;
-		A.action("status", function(err, apiResposne){
-			cb(err, apiResposne)
-		});
+      var cb = this.callback;
+    A.action("status", function(err, apiResposne){
+      cb(err, apiResposne)
+    });
        },
     'resp (simple)?' : function(res, apiResposne){ 
-    	assert.isObject(apiResposne);
-    	assert.strictEqual(apiResposne.stats.uptimeSeconds > 0, true);
-    	assert.strictEqual(apiResposne.stats.socketServer.numberOfLocalActiveSocketClients == 1, true);
+      assert.isObject(apiResposne);
+      assert.strictEqual(apiResposne.uptime > 0, true);
+      assert.strictEqual(apiResposne.stats['connections:toatalActiveConnections'] == 1, true);
     }
   }
 });
@@ -172,15 +172,15 @@ suite.addBatch({
 suite.addBatch({
   "I can run an action (complex)": {
     topic: function(){
-    	var cb = this.callback;
-		  params = { key: "mykey", value: "myValue" };
-		  A.actionWithParams("cacheTest", params, function(err, apiResposne){
-			 cb(err, apiResposne)
-		  });
+      var cb = this.callback;
+      params = { key: "mykey", value: "myValue" };
+      A.actionWithParams("cacheTest", params, function(err, apiResposne){
+       cb(err, apiResposne)
+      });
     },
     'resp (complex)?' : function(res, apiResposne){ 
-    	assert.isObject(apiResposne);
-    	assert.strictEqual(apiResposne.cacheTestResults.loadResp.value, "myValue");
+      assert.isObject(apiResposne);
+      assert.strictEqual(apiResposne.cacheTestResults.loadResp.value, "myValue");
     }
   }
 });
@@ -188,20 +188,20 @@ suite.addBatch({
 suite.addBatch({
   "I get events for say": {
     topic: function(){
-    	var cb = this.callback;
-    	var used = false;
-    	A.on("say",function(msgBlock){
-    		if(used == false){
-				cb(true, msgBlock);
-			}
-			used = true;
-		});
-		api.chatRoom.socketRoomBroadcast(api, null, "TEST MESSAGE");
-       },
+      var cb = this.callback;
+      var used = false;
+      A.on("say",function(msgBlock){
+        if(used == false){
+          cb(true, msgBlock);
+        }
+      used = true;
+      });
+      api.chatRoom.socketRoomBroadcast(null, "TEST MESSAGE");
+    },
     'resp (complex)?' : function(res, msgBlock){ 
-    	assert.isObject(msgBlock);
-    	assert.strictEqual(msgBlock.message, "TEST MESSAGE");
-    	assert.strictEqual(msgBlock.from, 0); // default from ID
+      assert.isObject(msgBlock);
+      assert.strictEqual(msgBlock.message, "TEST MESSAGE");
+      assert.strictEqual(msgBlock.from, 0); // default from ID
     }
   }
 });
