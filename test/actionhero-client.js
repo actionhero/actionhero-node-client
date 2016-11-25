10f5a51da41f85a7ebfb6bf57813e5573173b556
Evan Tahler <evantahler@gmail.com>
2016-11-25 11:03:12 -0800

    standard and new AH prototype

--- test/actionhero-client.js
+++ test/actionhero-client.js
@@ -1,185 +1,187 @@
-var should = require("should");
-var setup = require(__dirname + "/setup.js").setup;
-var actionheroClient = require(__dirname + "/../lib/actionhero-client.js");
-var client;
+var should = require('should')
+var path = require('path')
+var setup = require(path.join(__dirname, '/setup.js')).setup
+var ActionheroClient = require(path.join(__dirname, '/../lib/actionhero-client.js'))
+var client
 
 var connectionParams = {
-  host: "0.0.0.0",
+  host: '0.0.0.0',
   port: setup.serverConfigChanges.servers.socket.port,
-  timeout: 1000,
-};
+  timeout: 1000
+}
 
+describe('integration', function () {
+  beforeEach(function (done) {
+    client = new ActionheroClient()
+    setup.startServer(function () {
+      client.connect(connectionParams, function () {
+        done()
+      })
+    })
+  })
 
-describe('integration', function(){  
-  
-  beforeEach(function(done){
-    client = new actionheroClient();
-    setup.startServer(function(){
-      client.connect(connectionParams, function(){
-        done();
-      });
-    });
-  });
+  it('actionhero server should have booted', function (done) {
+    setup.api.should.be.an.instanceOf(Object)
+    done()
+  })
 
-  it("actionhero server should have booted", function(done){
-    setup.api.should.be.an.instanceOf(Object);
-    done();
-  });
+  it('can get my connection details', function (done) {
+    client.detailsView(function (erroror, details, delta) {
+      should.not.exist(erroror)
+      details.status.should.equal('OK')
+      details.context.should.equal('response')
+      details.data.totalActions.should.equal(0)
+      details.data.pendingActions.should.equal(0)
+      done()
+    })
+  })
 
-  it("can get my connection details", function(done){
-    client.detailsView(function(err, details, delta){
-      should.not.exist(err);
-      details.status.should.equal("OK");
-      details.context.should.equal("response");
-      details.data.totalActions.should.equal(0);
-      details.data.pendingActions.should.equal(0);
-      done();
-    });
-  });
+  it('should log server messages internally', function (done) {
+    client.log.length.should.equal(2)
+    client.log[0].data.welcome.should.equal('Hello! Welcome to the actionhero api')
+    done()
+  })
 
-  it("should log server messages internally", function(done){
-    client.log.length.should.equal(2);
-    client.log[0].data.welcome.should.equal("Hello! Welcome to the actionhero api");
-    done();
-  });
+  it('should be able to set params', function (done) {
+    client.paramAdd('key', 'value', function (erroror, response) {
+      should.not.exist(erroror)
+      response.status.should.equal('OK')
+      client.paramsView(function (erroror, params) {
+        params.data.key.should.equal('value')
+        done()
+      })
+    })
+  })
 
-  it("should be able to set params", function(done){
-    client.paramAdd("key", "value", function(err, response){
-      should.not.exist(err);
-      response.status.should.equal("OK");
-      client.paramsView(function(err, params){
-        params.data.key.should.equal("value");
-        done();
-      });
-    });
-  });
+  it('can delete params and confirm they are gone', function (done) {
+    client.paramAdd('key', 'value', function (erroror, response) {
+      should.not.exist(erroror)
+      client.paramsDelete(function (erroror, response) {
+        should.not.exist(erroror)
+        response.status.should.equal('OK')
+        client.paramsView(function (erroror, params) {
+          should.not.exist(erroror)
+          should.not.exist(params.data.key)
+          done()
+        })
+      })
+    })
+  })
 
-  it("can delete params and confirm they are gone", function(done){
-    client.paramAdd("key", "value", function(err, response){
-      should.not.exist(err);
-      client.paramsDelete(function(err, response){
-        should.not.exist(err);
-        response.status.should.equal("OK");
-        client.paramsView(function(err, params){
-          should.not.exist(err);
-          should.not.exist(params.data.key);
-          done();
-        });
-      });
-    });
-  });
+  it('can delete a param and confirm they are gone', function (done) {
+    client.paramAdd('key', 'v1', function (erroror, response) {
+      should.not.exist(erroror)
+      client.paramAdd('value', 'v2', function (erroror, response) {
+        should.not.exist(erroror)
+        client.paramDelete('key', function (erroror, response) {
+          should.not.exist(erroror)
+          client.paramsView(function (erroror, params) {
+            Object.keys(params.data).length.should.equal(1)
+            params.data.value.should.equal('v2')
+            done()
+          })
+        })
+      })
+    })
+  })
 
-  it("can delete a param and confirm they are gone", function(done){
-    client.paramAdd("key", "v1", function(err, response){
-      should.not.exist(err);
-      client.paramAdd("value", "v2", function(err, response){
-        should.not.exist(err);
-        client.paramDelete("key", function(err, response){
-          should.not.exist(err);
-          client.paramsView(function(err, params){
-            Object.keys( params.data ).length.should.equal(1);
-            params.data.value.should.equal("v2");
-            done();
-          });
-        });
-      });
-    });
-  });
+  it('can run an action (simple params)', function (done) {
+    client.action('status', function (erroror, apiResponse) {
+      should.not.exist(erroror)
+      apiResponse.uptime.should.be.above(0)
+      apiResponse.context.should.equal('response')
+      done()
+    })
+  })
 
-  it("can run an action (simple params)", function(done){
-    client.action("status", function(err, apiResponse){
-      should.not.exist(err);
-      apiResponse.uptime.should.be.above(0);
-      apiResponse.context.should.equal("response");
-      done();
-    });
-  });
+  it('can run an action (complex params)', function (done) {
+    var params = { key: 'mykey', value: 'myValue' }
+    client.actionWithParams('cacheTest', params, function (erroror, apiResponse) {
+      should.not.exist(erroror)
+      apiResponse.context.should.equal('response')
+      apiResponse.cacheTestResults.saveResp.should.equal(true)
+      done()
+    })
+  })
 
-  it("can run an action (complex params)", function(done){
-    var params = { key: "mykey", value: "myValue" };
-    client.actionWithParams("cacheTest", params, function(err, apiResponse){
-      should.not.exist(err);
-      apiResponse.context.should.equal("response");
-      apiResponse.cacheTestResults.saveResp.should.equal(true);
-      done();
-    });
-  });
+  it('can join a room', function (done) {
+    client.roomAdd('defaultRoom', function (erroror, data) {
+      client.roomView('defaultRoom', function (erroror, data) {
+        Object.keys(data.data.members).length.should.equal(1)
+        Object.keys(data.data.members)[0].should.equal(client.id)
+        done()
+      })
+    })
+  })
 
-  it("can join a room", function(done){
-    client.roomAdd('defaultRoom', function(err, data){
-      client.roomView('defaultRoom', function(err, data){
-        Object.keys( data.data.members ).length.should.equal(1);
-        Object.keys( data.data.members )[0].should.equal( client.id );
-        done();
-      });
-    });
-  });
+  it('can leave a room', function (done) {
+    client.detailsView(function (error, data) {
+      should.not.exist(error)
+      data.data.rooms.length.should.equal(0)
+      client.roomAdd('defaultRoom', function (error, data) {
+        should.not.exist(error)
+        client.roomView('defaultRoom', function (error, data) {
+          should.not.exist(error)
+          Object.keys(data.data.members).should.containEql(client.id)
 
-  it("can leave a room", function(done){
-    client.detailsView(function(err, data){
-      data.data.rooms.length.should.equal(0);
-      client.roomAdd('defaultRoom', function(err, data){
-        client.roomView('defaultRoom', function(err, data){
-          Object.keys( data.data.members ).should.containEql( client.id );
-          
-          client.roomLeave('defaultRoom', function(err, data){
-            client.detailsView(function(err, data){
-              data.data.rooms.length.should.equal(0);
-              done();
-            });
-          });
+          client.roomLeave('defaultRoom', function (error, data) {
+            should.not.exist(error)
+            client.detailsView(function (error, data) {
+              should.not.exist(error)
+              data.data.rooms.length.should.equal(0)
+              done()
+            })
+          })
+        })
+      })
+    })
+  })
 
-        });
-      });
-    });
-  });
+  it('will translate bad status to an error callback', function (done) {
+    client.roomView('someCrazyRoom', function (error, data) {
+      String(error).should.equal('Error: not member of room someCrazyRoom')
+      data.status.should.equal('not member of room someCrazyRoom')
+      done()
+    })
+  })
 
-  it('will translate bad status to an error callback', function(done){
-    client.roomView('someCrazyRoom', function(err, data){
-      String(err).should.equal('Error: not member of room someCrazyRoom');
-      data.status.should.equal('not member of room someCrazyRoom');
-      done();
-    });
-  });
-
-  it("will get SAY events", function(done){
-    var used = false;
-    client.roomAdd('defaultRoom', function(){
-      client.on("say",function(msgBlock){
-        if(used === false){
-          used = true;
-          msgBlock.message.should.equal("TEST MESSAGE");
-          done();
+  it('will get SAY events', function (done) {
+    var used = false
+    client.roomAdd('defaultRoom', function () {
+      client.on('say', function (msgBlock) {
+        if (used === false) {
+          used = true
+          msgBlock.message.should.equal('TEST MESSAGE')
+          done()
         }
-      });
+      })
 
-      setup.api.chatRoom.broadcast({}, 'defaultRoom', "TEST MESSAGE");
-    });
-  });
+      setup.api.chatRoom.broadcast({}, 'defaultRoom', 'TEST MESSAGE')
+    })
+  })
 
-  it('will obey the servers simultaneousActions policy', function(done){
-    client.actionWithParams("sleepTest", {sleepDuration: 500});
-    client.actionWithParams("sleepTest", {sleepDuration: 500});
-    client.actionWithParams("sleepTest", {sleepDuration: 500});
-    client.actionWithParams("sleepTest", {sleepDuration: 500});
-    client.actionWithParams("sleepTest", {sleepDuration: 500});
-    client.actionWithParams("sleepTest", {sleepDuration: 500}, function(err, apiResponse){
-      String(err).should.equal('Error: you have too many pending requests');
-      apiResponse.error.should.equal('you have too many pending requests');
-      done();
-    });
-  });
+  it('will obey the servers simultaneousActions policy', function (done) {
+    client.actionWithParams('sleepTest', {sleepDuration: 500})
+    client.actionWithParams('sleepTest', {sleepDuration: 500})
+    client.actionWithParams('sleepTest', {sleepDuration: 500})
+    client.actionWithParams('sleepTest', {sleepDuration: 500})
+    client.actionWithParams('sleepTest', {sleepDuration: 500})
+    client.actionWithParams('sleepTest', {sleepDuration: 500}, function (error, apiResponse) {
+      String(error).should.equal('Error: you have too many pending requests')
+      apiResponse.error.should.equal('you have too many pending requests')
+      done()
+    })
+  })
 
-  it("will obey timeouts", function(done){
-    client.actionWithParams("sleepTest", {sleepDuration: 2 * 1000}, function(err, apiResponse){
-      String( err ).should.equal('Error: Timeout reached');
-      should.not.exist(apiResponse);
-      done();
-    });
-  });
+  it('will obey timeouts', function (done) {
+    client.actionWithParams('sleepTest', {sleepDuration: 2 * 1000}, function (error, apiResponse) {
+      String(error).should.equal('Error: Timeout reached')
+      should.not.exist(apiResponse)
+      done()
+    })
+  })
+})
 
-});
-
-describe('connection and reconnection', function(){
+describe('connection and reconnection', function () {
   // TODO
-});
+})
--- test/setup.js
+++ test/setup.js
@@ -1,16 +1,16 @@
 exports.setup = {
-  serverPrototype: require("../node_modules/actionhero/actionhero.js").actionheroPrototype,
+  ServerPrototype: require('../node_modules/actionhero/actionhero.js'),
   serverConfigChanges: {
     general: {
-      id: "test-server-1",
+      id: 'test-server-1',
       workers: 1,
       developmentMode: false,
       startingChatRooms: {
         'defaultRoom': {},
-        'otherRoom': {},
-      },
+        'otherRoom': {}
+      }
     },
-    logger: { transports: null, },
+    logger: { transports: null },
     // logger: {
     //   transports: [
     //     function(api, winston){
@@ -28,31 +28,33 @@ exports.setup = {
       socket: {
         enabled: true,
         secure: false,
-        port: 9000,
-      },
+        port: 9000
+      }
     }
   },
 
-  startServer: function(callback){
-    var self = this;
+  startServer: function (callback) {
+    var self = this
 
-    if(!self.server){
-      process.env.ACTIONHERO_CONFIG = process.cwd() + "/node_modules/actionhero/config/";
-      self.server = new self.serverPrototype();
-      self.server.start({configChanges: self.serverConfigChanges}, function(err, api){
-        self.api = api;
-        callback(err, self.api);
-      });
-    }else{
-      process.nextTick(function(){
-        callback();
-      });
+    if (!self.server) {
+      process.env.ACTIONHERO_CONFIG = process.cwd() + '/node_modules/actionhero/config/'
+      self.server = new self.ServerPrototype()
+      self.server.start({configChanges: self.serverConfigChanges}, function (err, api) {
+        self.api = api
+        callback(err, self.api)
+      })
+    } else {
+      process.nextTick(function () {
+        callback()
+      })
     }
   },
 
-  stopServer: function(callback){
-    self.server.stop(function(){
-      callback();
-    });
-  },
-};
+  stopServer: function (callback) {
+    var self = this
+
+    self.server.stop(function () {
+      callback()
+    })
+  }
+}
